7e779931d5209407534074c1ad828480ed2c7ce9
Jakub Bartmiński <jakub.bartm@gmail.com>
2017-12-26 19:35:13 +0100

    Fix a bug in the RandomTriangles Block
    Fix a bug when a triangle would be split at a point outside that
    triangle, which caused overlapping triangles.

--- headers/RandomTriangles.hh
+++ headers/RandomTriangles.hh
@@ -3,15 +3,22 @@
 #include "Block.hh"
 #include "Tools.hh"
 
+/*
+ * RandomTriangles Block
+ *   Splits the image into random triangles
+ *	 (As of now they are not really random, this block will probably be deleted)
+ */
+
 class RandomTriangles: public Block
 {
 public:
 	Input<sf::Vector2u>* in_size;
 
-	/* Vector of triangles */
 	Output<std::vector<Triangle>>* out_triangles;
+
 protected:
 	virtual void Compute() override;
+	
 public:
 	RandomTriangles(std::string _name, Logger*  _logger);
 };
--- sources/RandomTriangles.cc
+++ sources/RandomTriangles.cc
@@ -9,47 +9,51 @@ void RandomTriangles::Compute()
 	int x = size.x;
 	int y = size.y;
 
-	std::list<Triangle> tris;
-	// random point inside instead?
-	tris.emplace_back(
+	std::list<Triangle> triangle_list;
+	// Not completely random - the triangles are made by splitting, 
+	// starting with one of the diagonals
+	triangle_list.emplace_back(
 			sf::Vector2f(0,0),
 			sf::Vector2f(0,y),
 			sf::Vector2f(x,0)
 	);
-	tris.emplace_back(
+	triangle_list.emplace_back(
 			sf::Vector2f(x,y),
 			sf::Vector2f(x,0),
 			sf::Vector2f(0,y)
 	);
 
-	const int iterations = 100;
+	const int iterations = 80;
 	for(int i = 0; i < iterations; ++i)
 	{
-		Triangle tri = tris.front();
-		tris.pop_front();
+		Triangle triangle = triangle_list.front();
+		triangle_list.pop_front();
 		sf::Vector2f p1, p2, p3;
-		p1 = tri[0].position;
-		p2 = tri[1].position;
-		p3 = tri[2].position;
-		sf::Vector2f p12 = {random_float(p1.x, p2.x), random_float(p1.y, p2.y)};
-		sf::Vector2f p23 = {random_float(p2.x, p3.x), random_float(p2.y, p3.y)};
-		sf::Vector2f p31 = {random_float(p3.x, p1.x), random_float(p3.y, p1.y)};
-		sf::Vector2f p4 = {(p12.x+p23.x+p31.x)/3, (p12.y+p23.y+p31.y)/3};
+		p1 = triangle[0].position;
+		p2 = triangle[1].position;
+		p3 = triangle[2].position;
+		float s1 = random_float(0, 1);
+		float s2 = random_float(0, 1);
+		float s3 = random_float(0, 1);
+		sf::Vector2f p12 = {p1.x + s1 * (p2.x - p1.x), p1.y + s1 * (p2.y - p1.y)};
+		sf::Vector2f p23 = {p2.x + s2 * (p3.x - p2.x), p2.y + s2 * (p3.y - p2.y)};
+		sf::Vector2f p31 = {p3.x + s3 * (p1.x - p3.x), p3.y + s3 * (p1.y - p3.y)};
+		sf::Vector2f p4 = {(p12.x + p23.x + p31.x) / 3, (p12.y + p23.y + p31.y) / 3};
 		Triangle t1 = Triangle(p1, p2, p4);
 		Triangle t2 = Triangle(p2, p3, p4);
 		Triangle t3 = Triangle(p3, p1, p4);
-		tris.push_back(t1);
-		tris.push_back(t2);
-		tris.push_back(t3);
+		triangle_list.push_back(t1);
+		triangle_list.push_back(t2);
+		triangle_list.push_back(t3);
 	}
 
-	for (Triangle &tri: tris)
-		tri.SetColor(sf::Color(random_float(0,255), random_float(0,255), random_float(0,255)));
+	for (Triangle &triangle: triangle_list)
+		triangle.SetColor(sf::Color(random_float(0,255), random_float(0,255), random_float(0,255)));
 
-	std::vector<Triangle> tris_vec;
-	for (Triangle &tri: tris)
-		tris_vec.push_back(tri);
-	SetData(out_triangles, tris_vec);
+	std::vector<Triangle> triangle_vector;
+	for (Triangle &triangle: triangle_list)
+		triangle_vector.push_back(triangle);
+	SetData(out_triangles, triangle_vector);
 }
 
 RandomTriangles::RandomTriangles(std::string _name, Logger* _logger)
