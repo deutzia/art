02cc5cc64c5247456dc4980bdea004dde07be169
Jakub Bartmiński <jakub.bartm@gmail.com>
2018-01-02 19:16:01 +0100

    Add the SideBySideCompare block
    The SideBySideCompare block can be used to render images of the
    same dimensions next to each other, for easy comparison.
    
    Adds the scale_texture function to Tools
    
    Fixes the ImageDrawer block to scale down oversized images

--- headers/ImageDrawer.hh
+++ headers/ImageDrawer.hh
@@ -3,11 +3,19 @@
 #include <SFML/Window.hpp>
 #include "Block.hh"
 
+/*
+ * ImageDrawer Block
+ *   Displays a texture in a window
+ *   The window is scaled if it's too large (the texture itself is unmodified)
+ *   The window has to be closed manually to continue execution
+ */
+
 class ImageDrawer: public Block
 {
-	sf::RenderWindow* window;
+	// Limits the size of the window
+	const float MAX_SCREEN_COVERAGE = .90;
 public:
-	Input<sf::Texture>* in_picture;
+	Input<sf::Texture>* in_texture;
 protected:
 	virtual void Compute() override;
 public:
