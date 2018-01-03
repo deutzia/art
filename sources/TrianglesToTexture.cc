4aec50cd46be6b5990f59beb3ffad02c6d6b781b
Iwona Kotlarska <pinus314@gmail.com>
2018-01-03 16:56:42 +0100

    Change from spaces to tabs in TrianglesToTexture

--- sources/TrianglesToTexture.cc
+++ sources/TrianglesToTexture.cc
@@ -2,29 +2,29 @@
  
 void TrianglesToTexture::Compute()
 {
-    logger->Enter("Computing Block TrianglesToTexture");
+	logger->Enter("Computing Block TrianglesToTexture");
  
-    sf::Vector2u size = in_size->GetData();
-    std::vector<Triangle> triangles = in_triangles->GetData();
+	sf::Vector2u size = in_size->GetData();
+	std::vector<Triangle> triangles = in_triangles->GetData();
  
-    sf::RenderTexture render_texture;
-    render_texture.create(size.x, size.y);
-    render_texture.clear();
+	sf::RenderTexture render_texture;
+	render_texture.create(size.x, size.y);
+	render_texture.clear();
  
-    for (Triangle &triangle: triangles)
-        render_texture.draw(triangle);
+	for (Triangle &triangle: triangles)
+		render_texture.draw(triangle);
  
-    render_texture.display(); // This fixes upside down textures
-    SetData(out_texture, render_texture.getTexture());
+	render_texture.display(); // This fixes upside down textures
+	SetData(out_texture, render_texture.getTexture());
  
-    logger->Exit();
+	logger->Exit();
 }
  
 TrianglesToTexture::TrianglesToTexture(std::string _name, Logger* _logger)
 : Block(_name, _logger)
 {
-    in_triangles = new Input<std::vector<Triangle>>(this);
-    in_size = new Input<sf::Vector2u>(this);
+	in_triangles = new Input<std::vector<Triangle>>(this);
+	in_size = new Input<sf::Vector2u>(this);
  
-    out_texture = new Output<sf::Texture>(this);
+	out_texture = new Output<sf::Texture>(this);
 }
